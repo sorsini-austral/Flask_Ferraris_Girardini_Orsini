5ac7406707b116915cf0c079650ad57b0b2458dc
sorsini-austral <sorsini@mail.austral.edu.ar>
2026-04-28 14:18:26 -0300

    initial setup

---
+++ create_bd.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:///database.db')
+df = pd.read_csv('data/instagram data.csv')
+df.to_sql('reels', engine, if_exists='replace', index=False)
+
+print("Base de datos creada!")
